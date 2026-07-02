e782e3e4d8887c1aa3ecb99e42f181ae34a66cc1
Anthony Tuininga <anthony.tuininga@gmail.com>
2026-07-02 09:49:30 -0600

    Bump version in preperation for new changes.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -53,10 +53,10 @@ author = 'Oracle'
 # |release|, also used in various other places throughout the built documents
 #
 # the short X.Y version
-version = '6.0'
+version = '6.1'
 
 # the full version, including alpha/beta/rc tags
-release = '6.0.0'
+release = '6.1.0b1'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
